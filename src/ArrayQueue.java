ab063167aee54e9e9f418f80c18a2cdc066914c0
SaitoAsuka <chezhongxun@live.cn>
2019-08-16 20:40:18 +0800

    简单实现Queue

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 public class ArrayQueue {
     private int start;
     private int end;
@@ -31,6 +33,18 @@ public class ArrayQueue {
         int temp=start;
         start = nextIndex(arr.length, start);
         return arr[temp];
+    }
+    public Integer pull(int obj){
+        if (size==initsize){
+            throw new ArrayIndexOutOfBoundsException("队列已满");
+        }
+        size++;
+        /*在队列中插入元素时，元素进入end指向位置，随后end变化
+        * 由于一开始就判断了队列是否为满
+        * 所以此处仅需要调用nextIndex方法来判断end是自增还是跳到数组开头*/
+        arr[end] = obj;
+        end = nextIndex(arr.length, end);
+
     }
     /*end和start需要循环，使用该方法进行判断*/
     public int nextIndex(int size,int index){
