933147cfbdac5e84531888ddc854b41226b866f1
SaitoAsuka <chezhongxun@live.cn>
2019-08-23 10:37:52 +0800

    归并排序

---
+++ src/example_marge.java
@@ -0,0 +1,42 @@
+public class example_marge {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 9, 7, 2, 1, 8};
+        marge(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void marge(int[] arr, int left_point, int right_point) {
+        /*由于在递归的时候left_point不一定为0，所以mid不能直接使用长度除以二*/
+        int mid = left_point + (right_point - left_point) / 2;
+        if (left_point == right_point) {
+            return;
+        }
+        marge(arr, left_point, mid);
+        marge(arr, mid + 1, right_point);
+        sort(arr, left_point, mid + 1, left_point, right_point);
+
+    }
+
+    public static void sort(int[] arr, int cur_left, int cur_right, int left_point, int right_point) {
+        int help[] = new int[arr.length];
+        int i = left_point;
+        int temp = (left_point+(right_point-left_point)/2);
+        while (cur_left <=temp  && cur_right <=right_point) {
+            help[i] = arr[cur_left] > arr[cur_right] ? arr[cur_right++] : arr[cur_left++];
+            i++;
+        }
+        while (cur_left <= temp) {
+            help[i] = arr[cur_left++];
+            i++;
+        }
+        while (cur_right <= right_point) {
+            help[i] = arr[cur_right++];
+            i++;
+        }
+        for (int j = left_point; j <= right_point; j++) {
+            arr[j] = help[j];
+        }
+    }
+}
