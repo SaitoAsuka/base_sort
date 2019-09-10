e83715de650a2bebcf109d839ec5cc037cd2ee6a
SaitoAsuka <chezhongxun@live.cn>
2019-09-10 17:34:12 +0800

    节点类

--- src/binary_tree/node.java
+++ src/binary_tree/node.java
@@ -1,10 +1,10 @@
 package binary_tree;
 
 public class node {
-    int data;
-    node parrent;
-    node left;
-    node right;
+    public int data;
+    public node parent;
+    public node left;
+    public node right;
 
     public node(int data) {
         this.data = data;
--- src/testForbinary_tree.java
+++ src/testForbinary_tree.java
@@ -1,9 +1,21 @@
 import binary_tree.node;
 
 public class testForbinary_tree {
-    public static void solution(node head){
+    public static node solution(node head) {
         node current = head;
-        if (current.)
+        if (current.right != null) {
+            current = current.right;
+            while (current.left != null) {
+                current = current.left;
+            }
+            return current;
+        }
+        node parent = current.parent;
+        while (parent != null && parent.right == null) {
+            current = current.parent;
+            parent = current.parent;
+        }
+        return current;
     }
 
 }
